e7afe176b71b81aa8b1a9f20272ae77ba8ed27fd
Daniel Baur <daniel.baur@uni-ulm.de>
2019-05-27 12:15:38 +0200

    fixed version

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-mvn deploy:deploy-file --settings="ci/settings.xml" -DgroupId=io.github.cloudiator.external -DartifactId=desmoj -Dversion=2.5.1e -Dpackaging=jar -Dfile=lib/desmoj-2.5.1e-bin.jar -DrepositoryId=ossrh -Durl=https://oss.sonatype.org/content/repositories/snapshots
+mvn deploy:deploy-file --settings="ci/settings.xml" -DgroupId=io.github.cloudiator.external -DartifactId=desmoj -Dversion=2.5.1e-SNAPSHOT -Dpackaging=jar -Dfile=lib/desmoj-2.5.1e-bin.jar -DrepositoryId=ossrh -Durl=https://oss.sonatype.org/content/repositories/snapshots
